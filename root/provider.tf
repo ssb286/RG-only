cdf078eb6e331981da184821ee7b4537a3c9b935
Sarvesh Baghel <ssb286@gmail.com>
2026-08-12 15:39:17 +0530

    Update provider.tf
    up

--- root/provider.tf
+++ root/provider.tf
@@ -5,16 +5,15 @@ terraform {
       version = "=5.0.1"
     }
   }
-    backend "azurerm" {
-    resource_group_name = "RG-Storage"
-    storage_account_name = "sarvstorageforbackend"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "statefilestorage"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "test.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+
+  backend "azurerm" {
+    resource_group_name  = "RG-Storage"
+    storage_account_name = "sarvstorageforbackend"
+    container_name       = "statefilestorage"
+    key                  = "test.terraform.tfstate"
   }
 }
 
-
 provider "azurerm" {
   features {}
-  
 }
